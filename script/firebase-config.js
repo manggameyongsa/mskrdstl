6831736901997ee4e960dc736e4f9e5d0e9f1d1d
manggameyongsa <jayler13579@gmail.com>
2026-03-04 08:13:13 +0900

    Create firebase-config.js

---
+++ script/firebase-config.js
@@ -0,0 +1,9 @@
+// export를 붙여야 다른 파일에서 "이거 빌려줘!" 하고 가져갈 수 있습니다.
+export const firebaseConfig = {
+  apiKey: "AIzaSyCmAeayjJDPHIA03uhxj38tfzg1VD57NTA",
+  authDomain: "yongsa-oc.firebaseapp.com",
+  projectId: "yongsa-oc",
+  storageBucket: "yongsa-oc.appspot.com",
+  messagingSenderId: "942437036530",
+  appId: "1:942437036530:web:7854c166440aa905cebb6b"
+};
